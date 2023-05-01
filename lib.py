a6bb98974d5fce51d6f93eee4c443bf3a50eadd9
luckk_diler <nob@dy.com>
2023-05-01 20:24:53 +0300

    baseOOP

--- lib.py
+++ lib.py
@@ -3,6 +3,16 @@ class Point:
     __y = 0
 
     def __init__(self, x ,y):
-        self.__x = y
+        self.__y = y
+        self.__x = x
+        print(self)
+
+    def getCoor(self):
+        return (self.__x,self.__y)
+
+    def setCoor(self, x ,y):
+        self.__y = y
         self.__x = x
 
+    def __del__(self):
+        print(self)
--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from lib import Point
 
 
 a = Point(10,20)
+b = Point(100,200)
+a.setCoor(40,30)
+print(b.getCoor())
+print(b.getCoor())
