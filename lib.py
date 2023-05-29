d900017d3e337aab6a55e1fd87518ede28efccd3
luckk_diler <nob@dy.com>
2023-05-29 19:41:33 +0300

    four

--- lib.py
+++ lib.py
@@ -16,3 +16,54 @@ class Point:
 
     def __del__(self):
         print(self)
+
+class Chess:
+
+    __x = 0
+    __y = 0
+    __MinCoor = 1
+    __MaxCoor = 8
+
+    def __init__(self,x,y,testCoor):
+        if testCoor(self,x):
+            self.__x = x
+        if testCoor(self, y):
+            self.__y = y
+
+
+    @classmethod
+    def testCoor(cls,value):
+        return Chess.__MinCoor <= value <= Chess.__MaxCoor
+
+    def getCoor(self):
+        return (self.__x,self.__y)
+
+    def setCoor(self, x, y):
+        self.__y = y
+        self.__x = x
+
+    def __getattribute__(self, item):
+        if item == "setCoor":
+            print('You are using setCoor')
+        return object.__getattribute__(self, item)
+
+    def __setattr__(self, key, value):
+        if key == '_Chess__x' or key == '_Chess_y':
+            if not self.testCoor(value):
+                raise Exception('no')
+        object.__setattr__(self,key,value)
+
+
+    def __getattr__(self, item):
+        print('getattr was called')
+
+    def __delattr__(self, item):
+        print('delattr was called')
+
+
+
+
+
+    @staticmethod
+    def gip(x,y):
+        return (x**2 + y**2)**0.5
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from lib import Point
+# from lib import Point
+from lib import Chess
+
+d = Chess(5,5)
+c = Chess(0,9)
 
 
 
-a = Point(10,20)
-b = Point(100,200)
-a.setCoor(40,30)
-print(b.getCoor())
-print(b.getCoor())
+# a = Point(10,20)
+# b = Point(100,200)
+# a.setCoor(40,30)
+# print(b.getCoor())
+# print(b.getCoor())
